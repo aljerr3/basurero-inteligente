9bea5cb8c7052c0d376c98784ba7ef8a88426948
Alejandro Rodríguez Radomile <82160143+aljerr3@users.noreply.github.com>
2026-01-02 17:08:31 -0300

    Delete OneDrive/Escritorio directory

--- OneDrive/Escritorio/chatvez/chatvez/src/components/InputChat.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function InputChat() {
-  return (
-        <input className='inputChat' placeholder='Escribe cualquier cosa' type="text"/>
-  )
-}
-
-export default InputChat
